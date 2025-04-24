2ef21aa1d3c22f685866167bdd19365e36abdaa1
Dominik Masson <dominik_25@outlook.de>
2025-04-24 17:50:33 +0200

    refactor: replace deprecated TemplateContextType constructor in SnippetsContext

--- src/main/kotlin/com/github/ginex25/riverpodx/snippets/SnippetsContext.kt
+++ src/main/kotlin/com/github/ginex25/riverpodx/snippets/SnippetsContext.kt
@@ -3,7 +3,7 @@ package com.github.ginex25.riverpodx.snippets
 import com.intellij.codeInsight.template.TemplateActionContext
 import com.intellij.codeInsight.template.TemplateContextType
 
-class SnippetsContext private constructor() : TemplateContextType("RIVERPOD_SNIPPETS", "snippets"){
+class SnippetsContext private constructor() : TemplateContextType("RIVERPOD SNIPPETS") {
     override fun isInContext(templateActionContext: TemplateActionContext): Boolean {
         return templateActionContext.file.name.endsWith(".dart")
     }
