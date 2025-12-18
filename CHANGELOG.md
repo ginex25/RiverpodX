cd53176783ba0ca656c0a7ec2c4dddd8ca5cbb6f
Dominik Masson <dominik_25@outlook.de>
2025-12-18 16:28:05 +0100

    chore: upgrade IntelliJ Gradle plugin to version 2.x

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,16 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## Unreleased
+
+### Build
+
+- Migrate to Gradle IntelliJ Plugin 2.x
+    - Updated build scripts to use `intellijPlatform {}` extension
+    - Replaced `runPluginVerifier` with `pluginVerification` DSL
+    - Configured signPlugin and publishPlugin tasks via `intellijPlatform`
+    - Removed deprecated `intellij {}` usage
+
 ## [1.3.0](https://github.com/ginex25/RiverpodX/releases/tag/v1.3.0) - 2025-08-29
 
 ### Added
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,57 +1,56 @@
 plugins {
     id("java")
     id("org.jetbrains.kotlin.jvm") version "1.9.25"
-    id("org.jetbrains.intellij") version "1.17.4"
+    id("org.jetbrains.intellij.platform") version "2.10.5"
 }
-
-group = "com.github.ginex25"
-version = "1.3.0"
 
 repositories {
     mavenCentral()
+    intellijPlatform {
+        defaultRepositories()
+    }
 }
 
-// Configure Gradle IntelliJ Plugin
-// Read more: https://plugins.jetbrains.com/docs/intellij/tools-gradle-intellij-plugin.html
-intellij {
-    version.set("2024.1.7")
-    type.set("IC")
 
-    plugins.set(listOf("org.jetbrains.android:241.17011.79", "io.flutter:82.1.2", "Dart:241.19416.15"))
+dependencies {
+    intellijPlatform {
+        create("IC", "2024.1")
+        plugins(providers.gradleProperty("platformPlugins").map { it.split(',') })
+        bundledPlugins(providers.gradleProperty("platformBundledPlugins").map { it.split(',') })
+    }
 }
+
+intellijPlatform {
+    pluginConfiguration {
+        id = "com.github.ginex25"
+        name = "RiverpodX"
+        version = "1.3.0"
+        ideaVersion {
+            sinceBuild = "241"
+        }
+    }
+    signing {
+        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
+        privateKey.set(System.getenv("PRIVATE_KEY"))
+        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
+    }
+    publishing {
+        token.set(System.getenv("PUBLISH_TOKEN"))
+    }
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
+}
+
 
 tasks {
-    // Set the JVM compatibility versions
     withType<JavaCompile> {
         sourceCompatibility = "17"
         targetCompatibility = "17"
     }
     withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
         kotlinOptions.jvmTarget = "17"
-    }
-
-    patchPluginXml {
-        sinceBuild.set("241")
-        untilBuild.set("252.*")
-    }
-
-    signPlugin {
-        certificateChain.set(System.getenv("CERTIFICATE_CHAIN"))
-        privateKey.set(System.getenv("PRIVATE_KEY"))
-        password.set(System.getenv("PRIVATE_KEY_PASSWORD"))
-    }
-
-    publishPlugin {
-        token.set(System.getenv("PUBLISH_TOKEN"))
-    }
-
-    runPluginVerifier {
-        ideVersions.set(
-            listOf(
-                "241.19416.15",
-                "251.23774.435",
-                "252.19874.12"
-            )
-        )
     }
 }
